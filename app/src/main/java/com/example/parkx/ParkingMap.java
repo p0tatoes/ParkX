7931c3f76d7f23a715d0be8a483cdde2b0fb78df
Kervi Asombrado <insertmailherelol@gmail.com>
2021-04-08 17:49:37 +0800

    bruhingus momentus

--- app/src/main/java/com/example/parkx/ParkingMap.java
+++ app/src/main/java/com/example/parkx/ParkingMap.java
@@ -2,14 +2,17 @@ package com.example.parkx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class ParkingMap extends AppCompatActivity {
+public class ParkingMap extends AppCompatActivity implements View.OnClickListener {
 
     TextView user_txt;
     String user;
-
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +22,20 @@ public class ParkingMap extends AppCompatActivity {
 
         user_txt = findViewById(R.id.user_txt);
         user_txt.setText(user);
+
+        button1 = findViewById(R.id.park1);
+        button1.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.park1:
+                button1.setEnabled(false);
+                break;
+        }
+
     }
 }
